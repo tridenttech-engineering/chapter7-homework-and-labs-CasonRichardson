d09ebdfc1ef0b1054f47a57654b3507e2de2e8ac
CasonRichardson <simpletheotter@gmail.com>
2025-02-25 09:33:45 -0500

    DONE

--- lab7-4.cpp
+++ lab7-4.cpp
@@ -12,10 +12,23 @@ double bill = 0.0;
 double totalBills = 0.0;
 int months = 0;
 
+
 cout << "Bill for month 1: ";
-	
-	
-	
-	
-	return 0;
-}	//end of main function
+cin >> bill;
+
+while (bill >= 0.0) {
+	months += 1;
+	totalBills += bill;
+	cout << "Bill for month " << months + 1 << ": ";
+	cin >> bill;
+}
+if (months > 0) {
+	avgBill = totalBills/months;
+	cout << fixed << setprecision(2);
+	cout << "Average electric bill for " << months << " months: $" << avgBill << endl;
+}
+else 
+	cout << "No bill amount entered." << endl;
+
+return 0;
+}//end of main function
